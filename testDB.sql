89d9eac032cc19afcd05a9ba74ebf3c421b87628
ylskt3 <ylskt3@mail.missouri.edu>
2016-03-25 20:12:00 -0500

    test cases

---
+++ testDB.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO user (username, password, name, email)
+VALUES ("test","pass","Yidong", "12345@missouri.edu");
+
+INSERT INTO user (username, password, name, email)
+VALUES ("test1","pass1","Zhihao", "adfaf@missouri.edu");
+
+INSERT INTO user (username, password, name, email)
+VALUES ("test2","pass2","Matt", "dafae@missouri.edu");
+
+INSERT INTO user (username, password, name, email)
+VALUES ("test3","pass3","Trenton", "dadffde@missouri.edu");
+
+INSERT INTO user (username, password, name, email)
+VALUES ("test4","pass4","Aren", "dafadafee@missouri.edu");
+
+INSERT INTO employer (business_name, password, email)
+VALUES ("MIZZOU","pass","MIZZOU@missouri.edu");
+
+INSERT INTO employer (business_name, password, email)
+VALUES ("MIZZOU1","pass","MIZZOU1missouri.edu");
+
+INSERT INTO employer (business_name, password, email)
+VALUES ("MIZZOU2","pass","MIZZOU2@missouri.edu");
