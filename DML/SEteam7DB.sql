83709e1934e857f0af83f42aacb41e28c042d0d0
tstwxc <tstwxc@mail.missouri.edu>
2016-04-07 15:14:18 -0500

    Update SEteam7DB.sql

--- DML/SEteam7DB.sql
+++ DML/SEteam7DB.sql
@@ -8,20 +8,23 @@ create table user(
     primary key(user_id)
 );
 
-DROP TABLE IF EXISTS employer;
-create table employer(
-    employer_id int not null auto_increment,
-    business_name varchar(20) not null,
-    password varchar(50) not null,
-    email varchar(100) not null,
-    primary key(employer_id)
+DROP TABLE IF EXISTS business;
+create table business(
+    business_id int not null auto_increment,
+    business_name varchar(30) not null,
+    location varchar(200) not null,
 );
 
+DROP TABLE IF EXISTS user_to_business;
+create table user_to_business(
+    business_name varchar(20) not null,
+    user_id int not null
+);
 
 DROP TABLE IF EXISTS experience;
 create table experience(
-	university_id int references education(university_id),
-	job_id int references job(job_id),
+    university_id int references education(university_id),
+    job_id int references job(job_id),
     company_name varchar(20) not null,
     title varchar(50) not null,
     location varchar(50) not null,
@@ -31,10 +34,10 @@ create table experience(
 
 DROP TABLE IF EXISTS education;
 create table education(
-	university_id int not null auto_increment,
+    university_id int not null auto_increment,
     university_name varchar(50) not null,
     location varchar(100) not null,
-    grade varchar(10) not null,
+    gpa float not null,
     dates_attended time not null,
     field_of_study varchar(50) not null,
     details varchar(200) not null,
@@ -43,16 +46,14 @@ create table education(
 
 DROP TABLE IF EXISTS job;
 create table job(
-	employer_id int references employer(employer_id),
-	location varchar(200) not null,
-	experience_level varchar(50) not null,
-    company_name varchar(20) not null,
+    business_id int references business(business_id),
+    experience_level varchar(50) not null,
     position varchar(20) not null,
     job_title varchar(20) not null,
     employer_type varchar(50) not null,
     job_poster varchar(50) not null,
-    job_description varchar(200) not null,
-    primary key(employer_id)
+    job_description varchar(500) not null,
+    primary key(business_id)
 );
 
 DROP TABLE IF EXISTS publications;
