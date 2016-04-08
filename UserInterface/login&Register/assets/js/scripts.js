b45d1ed5c6887123426131391e5a6165226b86e2
zzyrd <zzyrd@mail.missouri.edu>
2016-04-07 22:17:48 -0500

    Added files via upload

---
+++ UserInterface/login&Register/assets/js/scripts.js
@@ -0,0 +1,97 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch([
+                    "assets/img/backgrounds/4.jpg"
+	              , "assets/img/backgrounds/5.jpg"
+	              , "assets/img/backgrounds/8.jpg"
+	              , "assets/img/backgrounds/9.jpg"
+	              , "assets/img/backgrounds/10.jpg"
+	             ], {duration: 3000, fade: 750});
+    
+     /*
+	    Modals
+	*/
+	$('.launch-modal').on('click', function(e){
+		e.preventDefault();
+		$( '#' + $(this).data('modal-id') ).modal();
+	});
+    
+    
+    /*
+        Form validation of login
+    */
+    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.login-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    
+    /*
+        Form validation of registration
+    */
+    $('.registration-form input[type="text"], .registration-form input[type="password"], .registration-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.registration-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+});
+
+//check password during registration 
+function checkPass()
+{
+    //Store the password field objects into variables ...
+    var pass1 = document.getElementById('register-password');
+    var pass2 = document.getElementById('confirm-password');
+    //Store the Confimation Message Object ...
+    var message = document.getElementById('confirmMessage');
+    //Set the colors we will be using ...
+    var goodColor = "#00cc99";
+    var badColor = "#ff9999";
+    //Compare the values in the password field 
+    //and the confirmation field
+    if(pass1.value == pass2.value){
+        //The passwords match. 
+        //Set the color to the good color and inform
+        //the user that they have entered the correct password 
+        pass2.style.backgroundColor = goodColor;
+        message.style.color = goodColor;
+        message.innerHTML = "Passwords Match!"
+    }else{
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+        pass2.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = "Passwords Do Not Match!"
+    }
+}
